2e21060b57382c0fe80175c5bd5373720efb89a9
kimjongyoon <dangnary7@naver.com>
2026-06-02 15:47:20 +0900

    fix: nodemailer 정상 import 복원, @types/nodemailer 제거

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 interface InquiryData {
   name: string;
